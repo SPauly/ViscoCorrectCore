9d53fdd9e0cc81146d7958a57f213d32374392d5
S I M O N. P <spau@hotmail.de>
2024-03-01 14:49:19 +0100

    Add license header

--- include/spauly/vccore/correction_context.h
+++ include/spauly/vccore/correction_context.h
@@ -1,3 +1,20 @@
+// ViscoCorrectCore - Correction factors for centrifugal pumps
+// Copyright (C) 2024  Simon Pauly
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact via <https://github.com/SPauly/ViscoCorrectCore>
 #ifndef SPAULY_VCCORE_CORRECTION_CONTEXT_H_
 #define SPAULY_VCCORE_CORRECTION_CONTEXT_H_
 
