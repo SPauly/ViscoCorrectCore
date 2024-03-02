7e79a75d0f683b5f58513272727c7b4f297686d3
S I M O N. P <spau@hotmail.de>
2024-03-02 01:20:56 +0100

    Status

---
+++ include/spauly/vccore/calculator.h
@@ -0,0 +1,16 @@
+#ifndef SPAULY_VCCORE_CALCULATOR_H_
+#define SPAULY_VCCORE_CALCULATOR_H_
+
+namespace spauly {
+namespace vccore {
+
+class Calculator {
+ public:
+  Calculator() = default;
+  ~Calculator() = default;
+};
+
+}  // namespace vccore
+}  // namespace spauly
+
+#endif  // SPAULY_VCCORE_CALCULATOR_H_
--- src/correction_context.cpp
+++ src/correction_context.cpp
@@ -23,5 +23,20 @@ const CorrectionContext& CorrectionContext::operator=(
   return *this;
 }
 
+bool CorrectionContext::Initialize() {
+  std::lock_guard<std::mutex> lock(mux_.mux);
+  if (is_initialized_) return true;
+
+  // Open file
+  // Read csv in for the coefficients
+
+  // concurrently read the csv file for the graph coordinates and function
+
+  // concurrently read the init file with all input and output restrictions
+
+  // wait for the return of all functions and copy the data the the regarding
+  // storage
+}
+
 }  // namespace vccore
 }  // namespace spauly
