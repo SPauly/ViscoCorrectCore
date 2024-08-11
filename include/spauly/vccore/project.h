536d32b31ba2d8f703993ca4bc528d32e062a443
SPauly <spau@hotmail.de>
2024-08-11 21:22:14 +0200

    Update Project Layout

--- include/spauly/vccore/project.h
+++ include/spauly/vccore/project.h
@@ -23,219 +23,209 @@
 #include <string>
 #include <shared_mutex>
 
-#include "spauly/vccore/input_types.h"
-#include "spauly/vccore/impl/bundle_types.h"
-#include "spauly/vccore/impl/calculator.h"
+#include "spauly/vccore/data.h"
 
 namespace spauly {
 namespace vccore {
-// Forward declarations
-class Project;
-class CalculationCTX;
 
-/// Project is the main class of the library the user will interact with. It
-/// stores the input parameters and the correction factors. The calculation is
-/// done by the Calculator class. The Project class is thread-safe.
-/// Project expexts a CalculationCTX to be provided. The context holds necessary
-/// constants and helpers for the calculation and should be shared across
-/// multiple projects.
 class Project {
  private:
   using ReadLock = std::shared_lock<std::shared_mutex>;
   using WriteLock = std::unique_lock<std::shared_mutex>;
 
  public:
-  Project() = delete;
-  Project(std::shared_ptr<CalculationCTX> ctx);
-  Project(std::shared_ptr<CalculationCTX> ctx, InputT _flowrate, InputT _head,
-          InputT _viscosity, InputT _density,
-          FlowrateUnit _f_unit = FlowrateUnit::kCubicMetersPerHour,
-          HeadUnit _h_unit = HeadUnit::kMeters,
-          ViscosityUnit _v_unit = ViscosityUnit::kSquareMilPerSecond,
-          DensityUnit _d_unit = DensityUnit::kGramPerLiter);
+  Project() = default;
+  Project(InputT _flowrate, InputT _head, InputT _viscosity, InputT _density,
+          FlowrateUnit _f_unit = kStandardFlowrateUnit,
+          HeadUnit _h_unit = kStandardHeadUnit,
+          ViscosityUnit _v_unit = kStandardViscosityUnit,
+          DensityUnit _d_unit = kStandardDensityUnit);
 
   ~Project() = default;
 
   Project(const Project &other);
   Project &operator=(const Project &other);
 
-  /// Calculates the correction factors for the given input parameters. Calling
-  /// this before trying to access any of the correction factors is more
-  /// efficient. Returns false if the input parameters are invalid.
-  bool Calculate();
-
-  /// Converts the given input parameters to the internal representation. This
-  /// can be used to validate on which bases the correction factors were
+  /// @brief Converts the given input parameters to the internal representation.
+  /// This can be used to validate on which bases the correction factors were
   /// calculated.
-  const Project ShowConverted() const;
+  /// @return The converted input parameters.
+  const Parameters ShowConverted() const;
 
   // Getters
+
+  /// @brief Returns the q correction factor.
+  /// @return The q correction factor.
   double q();
+
+  /// @brief Returns the eta correction factor.
+  /// @return The h correction factor to return.
   double eta();
+
+  /// @brief Returns the h correction factors.
+  /// @return The h correction factors as an array. Where the index 0 = kH06, 1
+  /// = kH08, 2 = kH10, 3 = kH12.
   const std::array<double, 4> h();
+
+  /// @brief Returns the h correction factor for kH06.
+  /// @return The h correction factor for kH06.
   double h_06();
+
+  /// @brief Returns the h correction factor for kH08.
+  /// @return The h correction factor for kH08.
   double h_08();
+
+  /// @brief Returns the h correction factor for kH10.
+  /// @return The h correction factor for kH10.
   double h_10();
+
+  /// @brief Returns the h correction factor for kH12.
+  /// @return The h correction factor for kH12.
   double h_12();
 
-  /// Returns the name of the project.
-  inline const std::string &name() const {
-    ReadLock lock(mtx_);
-    return name_;
-  }
-
-  /// Returns the floating point precision for the calculations.
-  inline size_t floating_point_precision() const {
-    ReadLock lock(mtx_);
-    return floating_point_precision_;
-  }
-
-  /// Returns the ID of the project.
-  inline const size_t id() const {
-    ReadLock lock(mtx_);
-    return id_;
-  }
-
-  /// Returns whether the project has an error.
+  /// @brief Returns wheter there is an error.
+  /// @return True if there is an error, false otherwise.
   inline bool has_error() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return has_error_;
   }
 
-  /// Returns the flowrate.
+  /// @brief Returns the flowrate.
+  /// @return The flowrate.
   inline const InputT &flowrate() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return input_flowrate_;
   }
 
-  /// Returns the flowrate unit.
+  /// @brief Returns the flowrate unit.
+  /// @return The flowrate unit.
   inline const FlowrateUnit &flowrate_unit() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return flowrate_unit_;
   }
 
-  /// Returns the total head.
+  /// @brief Returns the total head.
+  /// @return The total head.
   inline const InputT &total_head() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return input_total_head_;
   }
 
-  /// Returns the head unit.
+  /// @brief Returns the head unit.
+  /// @return The head unit.
   inline const HeadUnit &head_unit() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return head_unit_;
   }
 
-  /// Returns the viscosity.
+  /// @brief Returns the viscosity.
+  /// @return The viscosity.
   inline const InputT &viscosity() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return input_viscosity_;
   }
 
-  /// Returns the viscosity unit.
+  /// @brief Returns the viscosity unit.
+  /// @return The viscosity unit.
   inline const ViscosityUnit &viscosity_unit() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return viscosity_unit_;
   }
 
-  /// Returns the density.
+  /// @brief Returns the density.
+  /// @return The density.
   inline const InputT &density() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return input_density_cp_;
   }
 
-  /// Returns the density unit.
+  /// @brief Returns the density unit.
+  /// @return The density unit.
   inline const DensityUnit &density_unit() const {
-    ReadLock lock(mtx_);
+    ReadLock lock(mux_);
     return density_unit_;
   }
 
-  // Setters
-
-  /// Sets the name of the project.
-  inline void set_name(const std::string &_name) {
-    WriteLock lock(mtx_);
-    name_ = _name;
-  }
-
-  /// Sets the floating point precision for the calculations.
-  void set_floating_point_precision(size_t _precision);
-
-  /// Sets the input parameters
+  /// @brief Sets the input parameters.
+  /// @param _flowrate The flowrate.
+  /// @param _head The total head.
+  /// @param _viscosity The viscosity.
+  /// @param _density The density.
+  /// @param _f_unit The flowrate unit.
+  /// @param _h_unit The head unit.
+  /// @param _v_unit The viscosity unit.
+  /// @param _d_unit The density unit.
   void Set(InputT _flowrate, InputT _head, InputT _viscosity, InputT _density,
-           FlowrateUnit _f_unit = FlowrateUnit::kCubicMetersPerHour,
-           HeadUnit _h_unit = HeadUnit::kMeters,
-           ViscosityUnit _v_unit = ViscosityUnit::kSquareMilPerSecond,
-           DensityUnit _d_unit = DensityUnit::kGramPerLiter);
+           FlowrateUnit _f_unit = kStandardFlowrateUnit,
+           HeadUnit _h_unit = kStandardHeadUnit,
+           ViscosityUnit _v_unit = kStandardViscosityUnit,
+           DensityUnit _d_unit = kStandardDensityUnit);
 
-  /// Sets the flowrate to the specified value.
+  /// @brief Sets the flowrate to the specified value.
+  /// @param _flowrate The flowrate value.
   void set_flowrate(const InputT &_flowrate);
-  /// Sets the flowrate unit to either m³/h, l/min or gpm.
+
+  /// @brief Sets the flowrate unit to either m³/h, l/min or gpm.
+  /// @param _unit The flowrate unit.
   void set_flowrate_unit(const FlowrateUnit &_unit);
 
-  /// Sets the total head to the specified value.
+  /// @brief Sets the total head to the specified value.
+  /// @param _head The total head value.
   void set_total_head(const InputT &_head);
-  /// Sets the head unit to either m or ft.
+
+  /// @brief Sets the head unit to either m or ft.
+  /// @param _unit The head unit.
   void set_head_unit(const HeadUnit &_unit);
 
-  /// Sets the viscosity to the specified value.
+  /// @brief Sets the viscosity to the specified value.
+  /// @param _viscosity The viscosity value.
   void set_viscosity(const InputT &_viscosity);
-  /// Sets the viscosity unit to either mm²/s, cSt, cP or mPa·s.
+
+  /// @brief Sets the viscosity unit to either mm²/s, cSt, cP or mPa·s.
+  /// @param _unit The viscosity unit.
   void set_viscosity_unit(const ViscosityUnit &_unit);
 
-  /// Sets the density to the specified value.
+  /// @brief Sets the density to the specified value.
+  /// @param _density The density value.
   void set_density(const InputT &_density);
-  /// Sets the density unit to either g/l or kg/m³.
+
+  /// @brief Sets the density unit to either g/l or kg/m³.
+  /// @param _unit The density unit.
   void set_density_unit(const DensityUnit &_unit);
 
  protected:
-  /// Helper function that resets the correction factors and metadata to
-  /// indicate an uncomputed change. Should be called whenever an input_ value
-  /// changes. Assumes that mtx_ is locked!
+  /// @brief Helper function that resets the correction factors and metadata to
+  /// indicate an uncomputed change. Should be called whenever an input value
+  /// changes. Assumes that mux_ is locked!
   void IndicateChange();
 
-  /// Helper function that handles the actual calculation. Assumes that mtx_ is
-  /// locked!
+  /// @brief Helper function that handles the actual calculation.
+  /// Assumes that mux_ is locked!
+  /// @return True if the calculation was successful, false otherwise.
   bool CalcImpl();
 
-  /// This wrapper ensures that the given read lock is unlocked and a write lock
-  /// is aquired before calling CalcImpl.
+  /// @brief This wrapper ensures that the given read lock is unlocked and a
+  /// write lock is acquired before calling CalcImpl.
+  /// @param lock The read lock.
+  /// @return True if the calculation was successful, false otherwise.
   bool GetterCalcWrapper(ReadLock &lock);
 
  private:
-  std::shared_ptr<CalculationCTX> ctx_ = nullptr;
-  const impl::Calculator calculator_;
-
-  mutable std::shared_mutex mtx_;
+  mutable std::shared_mutex mux_;
   bool was_computed_ = false;
   bool has_error_ = false;
-
   // Settings for the project
   std::string name_ = "";
-  size_t id_ = 0;
-  size_t floating_point_precision_ = 17;
 
   // Input parameters
   // This is used to avoid recomputing unchanged values. 0 = flowrate_, 1 =
   // total_head_, 2 = viscosity_, 3 = density_cp_
   std::array<bool, 4> was_changed_ = {true, true, true, true};
 
-  InputT input_flowrate_;
-  InputT input_total_head_;
-  InputT input_viscosity_;
-  /// Must be provided when the viscosity is given in centipoise.
-  InputT input_density_cp_;
+  Parameters input_;
+  Units units_;
 
-  FlowrateUnit flowrate_unit_ = FlowrateUnit::kCubicMetersPerHour;
-  HeadUnit head_unit_ = HeadUnit::kMeters;
-  ViscosityUnit viscosity_unit_ = ViscosityUnit::kSquareMilPerSecond;
-  DensityUnit density_unit_ = DensityUnit::kGramPerLiter;
-
-  // Converted Parameters
-  impl::ParametersInternal converted_input_;
-
-  // Result
-  impl::CorrectionFactors res_;
+  CorrectionFactors res_;
 
   // Computed helper functions for the correction factors
 };
